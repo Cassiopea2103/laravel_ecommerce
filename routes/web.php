349489a595595ace034dafc0e9886b782d9a6057
unknown <wadess@ept.sn>
2024-08-16 17:52:00 +0000

    Integrated templates

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ HomeController::class , 'home' ] ) -> name ( 'home' ) ;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])-> middleware(['auth' , 'user'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () { 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -24,3 +23,5 @@ require __DIR__.'/auth.php';
 // Admin routes : 
 Route::get ( '/admin/dashboard' , [ AdminController::class , 'index' ] ) -> name ('admin.dashboard')
             -> middleware ([ 'auth' , 'admin']) ; 
+
+        
